65d3fda6e62482008c1bf3845b5c97f67978f420
AZEUS\rafha <rafhall@hotmail.com>
2024-09-05 14:39:10 +0100

    experimenting with different edge colliders, script option + collider option

--- Assets/Scripts/WormMovement.cs
+++ Assets/Scripts/WormMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class WormMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 200f;
+
+    public float boundaryRadius = 10f; // same as the background radius
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // moving the snake forward
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+        // rotating the snake based on player input (A/D or Left/Right Arrow keys)
+        float rotation = Input.GetAxis("Horizontal") * -rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, rotation);
+
+        //StayWithinBoundary();
+    }
+
+    void StayWithinBoundary()
+    {
+        // calculate the distance from the center of the game area (0,0)
+        float distanceFromCenter = Vector2.Distance(Vector2.zero, transform.position);
+
+        // if the user worm is outside the boundary, move it back inside
+        if (distanceFromCenter > boundaryRadius)
+        {
+            // get the direction back towards the center
+            Vector2 directionToCenter = (Vector2.zero - (Vector2)transform.position).normalized;
+
+            // move the worm back inside the boundary
+            transform.position = (Vector2.zero + directionToCenter * boundaryRadius);
+        }
     }
 }
