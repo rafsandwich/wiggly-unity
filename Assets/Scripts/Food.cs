9099cca9f169002f312efbc9168f0c87e656d4d6
AZEUS\rafha <rafhall@hotmail.com>
2024-09-06 10:31:17 +0100

    add food script, change food spawning, fits in circle radius

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public float size; // storing the size of the food
+}
--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour
 {
     public GameObject foodPrefab;
-    public Vector2 spawnAreaMin;
-    public Vector2 spawnAreaMax;
+    //public Vector2 spawnAreaMin;
+    //public Vector2 spawnAreaMax;
+
+    public float spawnRadius = 10f; // radius of the game area circle
     public float spawnInterval = 2f;
+
+    public float minFoodSize = 0.05f;
+    public float maxFoodSize = 0.3f;
 
     private void Start()
     {
@@ -17,10 +22,18 @@ public class FoodSpawner : MonoBehaviour
     // Update is called once per frame
     private void SpawnFood()
     {
-        Vector2 spawnPosition = new Vector2(Random.Range(spawnAreaMin.x, spawnAreaMax.x), Random.Range(spawnAreaMin.y, spawnAreaMax.y));
+        //Vector2 spawnPosition = new Vector2(Random.Range(spawnAreaMin.x, spawnAreaMax.x), Random.Range(spawnAreaMin.y, spawnAreaMax.y));
 
+        // spawn in random position within the game area circus
+        Vector2 spawnPosition = Random.insideUnitCircle * spawnRadius;
+
+        // instantiate at the random position
         GameObject newFood = Instantiate(foodPrefab, spawnPosition, Quaternion.identity);
-        float randomSize = Random.Range(0.05f, 0.3f);
+        float randomSize = Random.Range(minFoodSize, maxFoodSize);
         newFood.transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        // store the size in a variable to pass on when eaten
+        newFood.GetComponent<Food>().size = randomSize;
+
     }
 }
